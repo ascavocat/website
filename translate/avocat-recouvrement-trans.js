4dceec747a434cb09f48ba2ce51c3d8f971d2368
ascavocat <avocatasc@gmail.com>
2023-08-12 12:17:14 -0400

    added avocat-recouvrement.html

---
+++ translate/avocat-recouvrement-trans.js
@@ -0,0 +1,188 @@
+var arrLang = {
+    "fr": {
+
+    // head
+    "TITLE": "Cabinet d'avocat en recouvrement",
+    "KEYWORDS": "avocat spécialisé en recouvrement, avocat en recouvrement, cabinet d'avocat recouvrement, avocat recouvrement Montréal, avocat recouvrement Laval, avocat recouvrement Longueuil, avocat en litige, avocat litige civil, avocat en litige commercial",
+
+    "DESCRIPTION": "Vous recherchez un avocat spécialisé en recouvrement? Faites appel aux services de ASC Avocat pour vous aider à récupérer vos créances impayées. Contactez-nous dès maintenant pour une assistance juridique efficace.",
+    
+
+    // body
+        // Navigation bar
+    "HOME": "Accueil",
+    "CABINET": "Notre cabinet",
+    "SERVICES": "Services",
+    "CONTACT": "Contact",
+    "CONSULTATION": "Consultation en ligne",
+        // Footer
+    "RIGHTS": "Services conçus et mis en oeuvre par ASC Avocat - Tous droits réservés",
+
+    "T1":"AVOCAT EN RECOUVREMENT",
+    "T2":"Des solutions juridiques adaptées selon vos besoins",
+    "T3":"Une représentation vigoureuse pour le recouvrement de vos créances",
+    "T4":"Des conseils stratégiques dans le but d'obtenir le meilleur resultat",
+    "T5":"Des services juridiques de recouvrement avec des tarifs compétitifs",
+    "T6":"Prendre un rendez-vous",
+    "CITY":"Montréal, Laval, Longueuil",
+    "PHONE":"(514) 600-5554",
+
+
+
+   
+
+    "T26":"CABINET D'AVOCAT SPÉCIALISÉ EN RECOUVREMENT DE CRÉANCES",
+    "T26-1":"Vous êtes à la recherche d'un avocat spécialisé en recouvrement de créances pour vous aider dans vos litiges financiers ? Ne cherchez plus, vous êtes au bon endroit !",
+
+    "T26-2":"Notre cabinet d'avocat a une expertise confirmée en droit des affaires et en recouvrement de créances. Que vous soyez une entreprise ou un particulier, nous mettons à votre disposition nos compétences et notre expérience afin de vous garantir un recouvrement rapide et efficace de vos créances.",
+
+    "T26-3":"Face aux difficultés de recouvrement de créances que vous rencontrez, il est essentiel de faire appel à un professionnel du droit pour défendre vos intérêts. Notre cabinet d'avocat vous accompagne tout au long de la procédure de recouvrement, depuis l'analyse de la situation juridique jusqu'à l'exécution des décisions de justice.",
+
+
+    "T27":"Nous intervenons dans tous les types de litiges de recouvrement : impayés de factures, recouvrement de dettes, contentieux bancaires, procédures de saisie, etc. Nous mettons en place une stratégie sur mesure pour chaque cas, afin de maximiser les chances de recouvrement de vos créances.",
+    
+    "T28":"Notre cabinet est ouvert à mettre en oeuvre la justice participative dans le cadre du recouvrement. Nous lançons des démarches de négociation préjudiciaire afin d'obtenir des accords amiables avec les débiteurs. Si ces tentatives échouent, nous engageons des procédures judiciaires pour récupérer votre argent.",
+
+    "T29":"Nous sommes parfaitement conscients des enjeux financiers auxquels vous faites face. C'est pourquoi nous mettons tout en œuvre pour réduire les délais de recouvrement et récupérer les sommes qui vous sont dues dans les plus brefs délais.",
+
+    "T30":"En faisant appel à notre cabinet d'avocats spécialisé en recouvrement, vous bénéficierez d'un accompagnement personnalisé et de conseils juridiques avisés. Notre équipe sera à vos côtés pour vous informer sur vos droits, élaborer une stratégie efficace et vous représenter devant les tribunaux si nécessaire.",
+
+    "T31":"Contactez-nous dès maintenant pour prendre rendez-vous et discuter de votre situation. Nous restons à votre disposition pour répondre à toutes vos questions et vous proposer la meilleure solution pour recouvrer vos créances.Faites confiance à notre expertise en recouvrement de créances et retrouvez sérénité et tranquillité financière.",
+
+
+
+
+    "T46":"Demander un rappel téléphonique",
+    "T47":"Laissez-nous vos informations, nous vous rappelerons aussitôt que possible pour cerner vos besoins juridiques.",
+    "T48":"Demander un rappel téléphonique",
+
+   
+
+    },
+    //**********************************************************************
+
+    "en": {
+
+    // head
+    "TITLE": "Debt Collection Law Firm",
+
+    "KEYWORDS": "Debt collection lawyer, collection lawyer, debt collection law firm, debt collection lawyer Montreal, debt collection lawyer Laval, debt collection lawyer Longueuil, litigation lawyer, civil litigation lawyer, commercial litigation lawyer",
+
+    "DESCRIPTION": "Looking for a debt collection lawyer? Let ASC Avocat help you recover your unpaid debts. Contact us now for effective legal representation.",
+    
+
+    // body
+        // Navigation bar
+    "HOME": "Home",
+    "CABINET": "Our law firm",
+    "SERVICES": "Services",
+    "CONTACT": "Contact",
+    "CONSULTATION": "Online consultation",
+        // Footer
+    "RIGHTS": "Services designed and implemented by ASC Avocat - All rights reserved",
+
+    "T1": " DEBT COLLECTION LAWYER",
+    "T2": "Legal solutions tailored to your needs",
+    "T3": "Vigorous representation to collect your debts",
+    "T4": "Strategic advice to achieve the best results",
+    "T5": " Debt collection legal services at competitive rates",
+    "T6": " Book an appointment",
+    "CITY": "Montreal, Laval, Longueuil",
+    "PHONE":"(514) 600-5554",
+
+    
+   
+    "T26": "DEBT COLLECTION LAW FIRM",
+    "T26-1": "Are you looking for a debt collection lawyer to help you with your financial disputes? Look no further, you've come to the right place!",
+
+    "T26-2": "Our law firm has proven expertise in business law and debt collection. Whether you're a company or an individual, we put our skills and experience at your disposal to ensure that your receivables are collected quickly and efficiently.",
+
+    "T26-3": "When faced with debt collection issues, it's essential to call on a legal professional to defend your interests. Our law firm will support you throughout the debt collection process, from analyzing the legal situation to enforcing court rulings.",
+
+
+    "T27": "We intervene in all types of collection disputes: unpaid invoices, debt collection, bank litigation, seizure procedures, etc. We put in place a strategy tailored to your needs. We implement a strategy tailored to each case, to maximize the chances of recovering your debts.",
+
+    "T28": "Our law firm is open to implementing collaborative approaches to debt collection. We initiate prejudicial negotiations to reach amicable agreements with debtors. If these attempts fail, we initiate legal proceedings to recover your money.",
+
+    "T29": "We are fully aware of the financial challenges you face. That's why we do everything we can to reduce collection times and recover the sums owed to you as quickly as possible.",
+
+    "T30": "By calling on our law firm specialized in debt collection, you will benefit from personalized support and sound legal advice. Our team will be at your side to inform you of your rights, develop an effective strategy and represent you in court if necessary.",
+
+    "T31": "Contact us now to make an appointment to discuss your situation. We'll be happy to answer any questions you may have, and offer you the best solution for recovering your debts. Put your trust in our debt collection expertise and regain your financial peace of mind.",
+
+
+    "T46": "Request a call back",
+    "T47": "Leave us your information and we'll call you back as soon as possible to discuss your legal needs.",
+    "T48": "Request a call back",
+  
+ 
+    }
+  };
+
+// Function to choose iframe based on language
+
+function go(url){
+  document.getElementById('JotFormIFrame-231485056021246').src= url;
+}
+
+ 
+// The default language is English
+var lang = "fr";
+// Check for localStorage support
+if('localStorage' in window){
+
+var usrLang = localStorage.getItem('uiLang');
+if(usrLang) {
+ lang = usrLang
+}
+
+}
+
+// handling request for appropriate form based on user language
+if (lang=== "fr"){
+  go('https://form.jotform.com/231485056021246?language=fr-CA&dropdown=hidden') ;
+
+} else if (lang=== "en"){
+  go('https://form.jotform.com/231485056021246?language=en-CA&dropdown=hidden') ;
+}  
+
+// end of handling above request
+
+
+console.log(lang);
+
+  $(document).ready(function() {
+
+    $(".lang").each(function(index, element) {
+      $(this).text(arrLang[lang][$(this).attr("key")]);
+    });
+  });
+
+  // get/set the selected language
+
+   $(".translate").click(function() {
+     var lang = $(this).attr("id");
+
+    // handling request for appropriate form based on user language
+if (lang=== "fr"){
+  go('https://form.jotform.com/231485056021246?language=fr-CA&dropdown=hidden') ;
+
+} else if (lang=== "en"){
+  go('https://form.jotform.com/231485056021246?language=en-CA&dropdown=hidden') ;
+}  
+
+// end of handling above request
+     
+
+
+    
+    // update localStorage key
+    if('localStorage' in window){
+         localStorage.setItem('uiLang', lang);
+         console.log( localStorage.getItem('uiLang') );
+    }
+
+    $(".lang").each(function(index, element) {
+      $(this).text(arrLang[lang][$(this).attr("key")]);
+    });
+  });
